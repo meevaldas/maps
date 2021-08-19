b66b4a4b51bcb43112d7f0c8a375e95e32a7b17f
Meeval Das <meevadas13@gmail.com>
2021-08-19 14:48:21 +0530

    add CustomMap.ts nd end of the project.

--- src/Company.ts
+++ src/Company.ts
@@ -1,19 +1,29 @@
 import * as faker from 'faker';
+import { Mappable } from './CustomMap'
 
-export class Company{
+export class Company implements Mappable{
     companyName:string;
     phrase: string;
-    companyLocation:{
+    location:{
         lat:number,
         lng:number
     }
+    color:string = 'red';
 
     constructor(){
         this.companyName = faker.company.companyName();
         this.phrase = faker.company.catchPhrase();
-        this.companyLocation = {
+        this.location = {
             lat:parseFloat(faker.address.latitude()),
             lng:parseFloat(faker.address.longitude())
-        }
+        };
+    }
+
+    markerContent():string{
+        return `
+            <div>
+                <h1>Company Name: ${ this.companyName }</h1>
+                <h3>Catchphrase: ${ this.phrase }</h3>
+            </div>`;
     }
 }
